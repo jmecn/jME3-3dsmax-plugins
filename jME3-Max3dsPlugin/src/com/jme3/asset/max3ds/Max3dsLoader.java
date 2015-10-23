15f6718af65e258c2d122bd78853eacf7e167fc0
windybell <115050813@qq.com>
2015-10-23 13:46:46 +0800

    First commit

---
+++ jME3-Max3dsPlugin/src/com/jme3/asset/max3ds/Max3dsLoader.java
@@ -0,0 +1,22 @@
+package com.jme3.asset.max3ds;
+
+import java.io.IOException;
+
+import com.jme3.asset.AssetInfo;
+import com.jme3.asset.AssetLoader;
+
+/**
+ * Max 3ds loader for jME3
+ * 
+ * @author yanmaoyuan
+ *
+ */
+public class Max3dsLoader implements AssetLoader {
+
+	@Override
+	public Object load(AssetInfo assetInfo) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
